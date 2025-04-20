bd121c15f6220c77f0a6dba973cf6cc0d98f96d7
TrucDeThuong <hvthanhtruc121203@gmail.com>
2025-04-20 22:49:08 +0700

    Add artist_management_view, genres_management_view

---
+++ api/views/admin/artist_management_view.py
@@ -0,0 +1,109 @@
+from api.models import Song, Artist, Genre
+from api.serializers import SongSerializer, ArtistSerializer, GenreSerializer
+from rest_framework.decorators import api_view, permission_classes, parser_classes
+from rest_framework.permissions import AllowAny
+from rest_framework.response import Response
+from rest_framework.parsers import MultiPartParser, FormParser
+from rest_framework import status
+from api.utils.decode_token import decode_token
+from math import ceil
+from django.http import JsonResponse
+from django.shortcuts import get_object_or_404
+import json
+from django.db import IntegrityError
+
+
+# artist
+# API get artists
+@api_view(["GET"])
+@permission_classes([AllowAny])
+def get_artists(request):
+    user_id, error_response = decode_token(request)
+    if error_response: return error_response
+    try:
+        page = int(request.GET.get("page", 1))
+        limit = int(request.GET.get("limit", 6))
+
+        offset = (page - 1) * limit
+        all_artists = Artist.objects.all().order_by("id")
+        total_count = all_artists.count()
+        page_count = ceil(total_count / limit)
+
+        artists = all_artists[offset : offset + limit]
+
+        # Serialize
+        serializer = ArtistSerializer(artists, many=True, context={"request": request})
+
+        return Response(
+            {"data": serializer.data, "count": total_count, "page_count": page_count},
+            status=status.HTTP_200_OK,
+        )
+    except Exception as e:
+        return Response({"error": str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
+
+@api_view(["POST"])
+@permission_classes([AllowAny])
+def add_artist(request):
+    if request.method == "POST":
+        user_id, error_response = decode_token(request)
+        if error_response: return error_response
+        try:
+            data = json.loads(request.body)
+            name = data.get("name")
+            if not name:
+                return JsonResponse(
+                    {"error": "Tên nghệ sĩ không được để trống"}, status=400
+                )
+            artist = Artist.objects.create(name=name)
+            return JsonResponse({"id": artist.id, "name": artist.name}, status=201)
+        except IntegrityError:
+            return JsonResponse({"error": "Nghệ sĩ đã tồn tại"}, status=400)
+        except Exception as e:
+            return JsonResponse({"error": str(e)}, status=500)
+    return JsonResponse({"error": "Phương thức không được hỗ trợ"}, status=405)
+
+
+@api_view(["PUT"])
+@permission_classes([AllowAny])
+def update_artist(request, id):
+    if request.method == "PUT":
+        user_id, error_response = decode_token(request)
+        if error_response: return error_response
+        try:
+            artist = get_object_or_404(Artist, id=id)
+            data = json.loads(request.body)
+            name = data.get("name")
+            if not name:
+                return JsonResponse(
+                    {"error": "Tên nghệ sĩ không được để trống"}, status=400
+                )
+            artist.name = name
+            artist.save()
+            return JsonResponse({"id": artist.id, "name": artist.name}, status=200)
+        except Exception as e:
+            return JsonResponse({"error": str(e)}, status=500)
+    return JsonResponse({"error": "Phương thức không được hỗ trợ"}, status=405)
+
+
+@api_view(["DELETE"])
+@permission_classes([AllowAny])
+def delete_artist(request, id):
+    if request.method == "DELETE":
+        user_id, error_response = decode_token(request)
+        if error_response: return error_response
+        try:
+            artist = get_object_or_404(Artist, id=id)
+            # Xóa tất cả bài hát liên quan đến nghệ sĩ
+            songs = Song.objects.filter(artist=artist)
+            if songs.exists():
+                songs.delete()
+            # Xóa nghệ sĩ
+            artist.delete()
+            return JsonResponse(
+                {"message": "Xóa nghệ sĩ và các bài hát liên quan thành công"},
+                status=200,
+            )
+        except Exception as e:
+            return JsonResponse({"error": str(e)}, status=500)
+    return JsonResponse({"error": "Phương thức không được hỗ trợ"}, status=405)
